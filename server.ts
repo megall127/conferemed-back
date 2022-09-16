b88e27da5e11798d8138fb2f152d5cf0e247b1be
Leandro Wilker Paulino Sousa <83305893+megall127@users.noreply.github.com>
2022-09-16 03:30:23 -0300

    ajustes

--- server.ts
+++ server.ts
@@ -16,4 +16,4 @@ import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
-new Ignitor(__dirname).httpServer().start()
+new Ignitor().httpServer().start()
